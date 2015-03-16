91f614d399b218c2b6ac9effeb66afa4056ec050
Thomas Frieling <tfrieling@thomass-mbp.speedport_w_723v_1_36_000>
2015-03-16 18:36:37 +0100

    behave: first useful test

---
+++ mysite/behave_tutorial/behave/steps/step_tutorial02.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------
+# STEPS:
+# ----------------------------------------------------------------------------
+from behave   import given, when, then
+from hamcrest import assert_that, equal_to, is_not
+from behave_tutorial.classes.NinjaFight import NinjaFight
+
+@given('the ninja has a {achievement_level}')
+def step_the_ninja_has_a(context, achievement_level):
+    context.ninja_fight = NinjaFight(achievement_level)
+#@given('the ninja has a third level black-belt')
+#def step_the_ninja_has_a(context):
+#    achievement_level = 'third level black-belt'
+#    context.ninja_fight = NinjaFight(achievement_level)
+
+@when('attacked by a {opponent_role}')
+def step_attacked_by_a(context, opponent_role):
+    context.ninja_fight.opponent = opponent_role
+
+@when('attacked by {opponent}')
+def step_attacked_by(context, opponent):
+    context.ninja_fight.opponent = opponent
+
+@then('the ninja should {reaction}')
+def step_the_ninja_should(context, reaction):
+    assert_that(reaction, equal_to(context.ninja_fight.decision()))
